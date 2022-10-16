9007192a678072131278d630a8c2973bc4c6f2c6
lincolnscott <72884965+lincolnscott@users.noreply.github.com>
2022-10-16 13:20:19 -0500

    Initial commit

---
+++ src/main/java/net/ljgram/lumpypegimus/LumpyPegimus.java
@@ -0,0 +1,15 @@
+package net.ljgram.lumpypegimus;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LumpyPegimus implements ModInitializer {
+
+	public static final String MOD_ID = "lumpypegimus";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/ljgram/lumpypegimus/LumpyPegimusClient.java
@@ -0,0 +1,10 @@
+package net.ljgram.lumpypegimus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LumpyPegimusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
